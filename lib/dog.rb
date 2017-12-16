e88130338254ef381fc3a069076df3d03a1c4dc9
ayesha ansari <ayesha.x.ansari@gmail.com>
2017-12-16 14:39:54 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+class Dog
+  def initialize(name, bark = "woof!")
+    @name = name
+    @bark = bark
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def bark=(bark)
+    @bark = bark
+  end
+
+  def bark
+    @bark
+  end
+end
+ fido = Dog.new("Fido")
